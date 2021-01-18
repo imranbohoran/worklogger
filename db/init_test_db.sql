54035b45a9d64b7caa5659af0804fc185f63da58
Imran Bohoran <imranbohoran@gmail.com>
2021-01-18 23:53:33 +0000

    Using docker image init script capability
    * Saves needing custom tasks on gradle to initialise the db

---
+++ db/init_test_db.sql
@@ -0,0 +1,5 @@
+DROP DATABASE IF EXISTS worklog_test;
+DROP ROLE IF EXISTS worklog_test;
+CREATE ROLE worklog_test WITH LOGIN PASSWORD 'worklog_test-pass';
+CREATE DATABASE worklog_test WITH OWNER worklog_test;
+GRANT ALL PRIVILEGES ON DATABASE worklog_test TO worklog_test;
